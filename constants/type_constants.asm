2cb04b97f23fe232eaa102f1dd0bd6af4ab55400
seph-bit <seraphim.beatle@gmail.com>
2026-02-25 19:49:32 -0600

    Update type_constants.asm

--- constants/type_constants.asm
+++ constants/type_constants.asm
@@ -10,19 +10,22 @@ DEF PHYSICAL EQU const_value
 	const ROCK         ; $05
 	const BIRD         ; $06
 	const BUG          ; $07
-	const GHOST        ; $08
+	const DRAGON       ; $1A
+    const DARK         ; $1B
+    const STEEL        ; $1C
 
 DEF UNUSED_TYPES EQU const_value
 	const_next 20
 DEF UNUSED_TYPES_END EQU const_value
 
 DEF SPECIAL EQU const_value
+    const GHOST.       ; $08
 	const FIRE         ; $14
 	const WATER        ; $15
 	const GRASS        ; $16
 	const ELECTRIC     ; $17
 	const PSYCHIC_TYPE ; $18
 	const ICE          ; $19
-	const DRAGON       ; $1A
+    const FAIRY        ; $1D
 
 DEF NUM_TYPES EQU const_value
